05490bcd8524b8b2b497e9959b002f947c519ed9
Igor Pchelko <igor.pchelko@gmail.com>
2016-02-20 09:51:43 +0100

    Initial Commit

---
+++ Assignment1/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Assignment1
+//
+//  Created by Igor Pchelko on 20/02/16.
+//  Copyright © 2016 Igor Pchelko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Assignment1/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Assignment1
+//
+//  Created by Igor Pchelko on 20/02/16.
+//  Copyright © 2016 Igor Pchelko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
